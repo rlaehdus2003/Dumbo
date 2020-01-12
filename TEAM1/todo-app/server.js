290cf41b79a127f8809823bbca27cd5e2812f325
Kimdoyeon <rlaehdus2004@gmail.com>
2020-01-12 22:18:24 +0900

    commit

--- TEAM1/todo-app/server.js
+++ TEAM1/todo-app/server.js
@@ -1,20 +1,61 @@
-var express = require("express")
-var path = require("path")
-var bodyParser = require("body-parser")
+//server.js
+//adding opensource modules to application
+var express = require('express'); //express
+var path = require('path'); //for refering physical files here
+var logger = require('morgan');
+var cookieParser = require('cookie-parser'); //for maintain sessions
+var bodyParser = require('body-parser'); //for parsing json
+var bcrypt = require('bcrypt-nodejs');
+var passport = require('passport'); //Using passportjs for authentication
+var LocalStrategy = require('passport-local').Strategy; //using passport strategy
+var session = require('express-session'); //for maintaining sessions
+var mongoose = require('mongoose'); //for mongodb, database
+var models_user = require('./Angular/Models/user.js'); // refering models in server.js
 
-var tasks = require("./routes/tasks")
-var cors = require("cors")
+//connection database
+mongoose.connect('mongodb://localhost/AngularizeApp');
 
-var port = 3000
+//import the routers
+var router = require('./Routes/router');
+var authenticate = require('./Routes/authentication')(passport);
 
-var app =express()
-app.use(cors())
+//for using express throughout this application
+var app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
-app.use("/api", tasks)
+//tell node that My application will use ejs engine for rendering, view engine setup
+app.set('views', path.join(__dirname, 'Views'));
+app.set('view engine', 'ejs');
 
-app.listen(port,function(){
-  console.log("Server started on port" + port)
+//tell node the global configuration about parser,logger and passport
+app.use(cookieParser());
+app.use(logger('dev'));
+app.use(session({
+  secret: 'keyboard cat'
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize()); //initializing passport
+app.use(passport.session()); //initializing passport session
 
-})
+//tell node about these directories that application may get resources from
+app.use('/', router);
+app.use('/auth', authenticate);
+app.use(express.static(path.join(__dirname, 'scripts')));
+app.use(express.static(path.join(__dirname, 'Content')));
+app.use(express.static(path.join(__dirname, 'Angular')));
+app.use(express.static(path.join(__dirname, 'Views/Main')));
+app.use(express.static(path.join(__dirname, 'Views/Authentication')));
+
+//providing auth-api to passport so that it can use it.
+var initPassport = require('./Passport/passport-init');
+initPassport(passport);
+
+//running server on node
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+//exporting this application as a module
+module.exports = app;
